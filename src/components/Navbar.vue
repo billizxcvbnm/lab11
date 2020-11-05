c06d501af0502b9650b2213437c41417057fc28d
billizxcvbnm <68525520+billizxcvbnm@users.noreply.github.com>
2020-11-05 10:32:54 +0700

    init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+<nav>
+    <div class="nav-wrapper blue">
+        <div class="container">
+            <router-link to="/" 
+            class="brand-logo">Employee
+            Manager</router-link>
+        </div>
+    </div>
+</nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyAGt5enoBxWpQdZIC7ixzojd-edqgdfGDM",
+    authDomain: "vuefs-prod-8f925.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-8f925.firebaseio.com",
+    projectId: "vuefs-prod-8f925",
+    storageBucket: "vuefs-prod-8f925.appspot.com",
+    messagingSenderId: "626193222976",
+    appId: "1:626193222976:web:61ddb4913a9469e6b23ea5",
+    measurementId: "G-R95JKEM3ZS"
+}
